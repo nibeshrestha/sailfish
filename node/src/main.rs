5e948fd5fd37bcc47a81be8c091684251c10adb8
Nibesh Shrestha <n.shrestha@supraoracles.com>
2025-02-13 19:54:35 -0500

    increase consensus channel capacity

--- node/src/main.rs
+++ node/src/main.rs
@@ -134,7 +134,7 @@ async fn run(matches: &ArgMatches<'_>) -> Result<()> {
         ("primary", _) => {
             let (tx_new_certificates, rx_new_certificates) = channel(CHANNEL_CAPACITY);
             let (tx_feedback, rx_feedback) = channel(CHANNEL_CAPACITY);
-            let (tx_consensus_header, rx_consensus_header) = channel(CHANNEL_CAPACITY);
+            let (tx_consensus_header, rx_consensus_header) = channel(4*CHANNEL_CAPACITY);
             Primary::spawn(
                 ed_keypair,
                 bls_keypair,
